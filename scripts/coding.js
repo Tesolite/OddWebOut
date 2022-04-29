3edf65ddfecfe457ff1acd617b5345520f99a27d
Tesolite <pavel.kotka@gmail.com>
2022-04-29 04:36:51 +0100

    Complete website implemented

--- scripts/coding.js
+++ scripts/coding.js
@@ -17,17 +17,17 @@ function saveOptions(){
     var darkmode = document.getElementById("darkmode")
     if(optionsWindow.style.display == "initial"){
         if(dyslexia.checked == true && darkmode.checked == true){
-            webstyle.setAttribute("href","/stylesheets/dyslexicDM.css");
+            webstyle.setAttribute("href","stylesheets/dyslexicDM.css");
             localStorage.setItem("dyslexicDM", 1);
         }
         else if(dyslexia.checked == true){
-            webstyle.setAttribute("href","/stylesheets/dyslexic.css");
+            webstyle.setAttribute("href","stylesheets/dyslexic.css");
             localStorage.setItem("dyslexic", 1);
             localStorage.setItem("darkmode", 0);
             localStorage.setItem("dyslexicDM", 0);
         }
         else if(darkmode.checked == true){
-            webstyle.setAttribute("href","/stylesheets/darkmode.css");
+            webstyle.setAttribute("href","stylesheets/darkmode.css");
             localStorage.setItem("darkmode", 1);
             localStorage.setItem("dyslexic", 0);
             localStorage.setItem("dyslexicDM", 0);
@@ -36,7 +36,7 @@ function saveOptions(){
             localStorage.setItem("dyslexicDM", 0);
             localStorage.setItem("dyslexic", 0);
             localStorage.setItem("darkmode", 0);
-            webstyle.setAttribute("href","/stylesheets/styling.css");
+            webstyle.setAttribute("href","stylesheets/styling.css");
         }
     }
         optionsWindow.style.display = "none";
@@ -44,27 +44,28 @@ function saveOptions(){
 
 function loadOptions(){
     if(localStorage.getItem("dyslexicDM") == 1){
-        webstyle.setAttribute("href","/stylesheets/dyslexicDM.css")
+        webstyle.setAttribute("href","stylesheets/dyslexicDM.css")
+        alert(window.location.pathname);
         if(window.location.pathname == "/index.html"){
         dyslexia.checked = true;
         darkmode.checked = true;
         }
     }
     else if(localStorage.getItem("dyslexic") == 1){
-        webstyle.setAttribute("href","/stylesheets/dyslexic.css");
+        webstyle.setAttribute("href","stylesheets/dyslexic.css");
         if(window.location.pathname == "/index.html"){
             dyslexia.checked = true;
             }
 
     }
     else if(localStorage.getItem("darkmode") == 1){
-        webstyle.setAttribute("href","/stylesheets/darkmode.css");
+        webstyle.setAttribute("href","stylesheets/darkmode.css");
         if(window.location.pathname == "/index.html"){
             darkmode.checked = true;
             }
     }
     else{
-        webstyle.setAttribute("href","/stylesheets/styling.css");
+        webstyle.setAttribute("href","stylesheets/styling.css");
     }
 }
 
@@ -72,7 +73,7 @@ function colourfulSecret(){
     var code = prompt("Experience the moment.");
     if(localStorage.getItem("dyslexicDM") == 1){
         if(code == "watch?v=OQSNhk5ICTI"){
-            webstyle.setAttribute("href","/stylesheets/rain.css")
+            webstyle.setAttribute("href","stylesheets/rain.css")
         }
     }
 }
